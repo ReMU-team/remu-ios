b3765cb040330d0d080c358a8b9d5429069cbf97
guingguing <kimjinseo0402@gmail.com>
2026-01-09 04:01:33 +0900

    chore: set color & font design system

---
+++ ReMU/ReMU/ReMUApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMUApp.swift
+//  ReMU
+//
+//  Created by 김진서 on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ReMUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReMU/ReMU/Resource/Font/FontExtension.swift
@@ -0,0 +1,19 @@
+//
+//  FontManager.swift
+//  ReMU
+//
+//  Created by 김진서 on 1/9/26.
+//
+
+import SwiftUI
+
+extension Font {
+    static let pt32 = Font.system(size: 32)
+    static let pt24 = Font.system(size: 24)
+    static let pt20 = Font.system(size: 20)
+    static let pt18 = Font.system(size: 18)
+    static let pt16 = Font.system(size: 16)
+    static let pt15 = Font.system(size: 15)
+    static let pt13 = Font.system(size: 13)
+    static let pt12 = Font.system(size: 12)
+}
