90dc6c5eea26a14554c34fef67c6d4777e1f4d1b
guingguing <kimjinseo0402@gmail.com>
2026-02-10 09:41:12 +0900

    feat: 로그인 세션 키체인 저

--- ReMU/ReMU/Modules/AppFlow/FlowView/RootView.swift
+++ ReMU/ReMU/Modules/AppFlow/FlowView/RootView.swift
@@ -9,27 +9,31 @@ import SwiftUI
 
 struct RootView: View {
     @StateObject private var appState = AppState()
+    @StateObject private var container: DIContainer = .init()
 
-
+    
     var body: some View {
         switch appState.route {
         case .splash:
             SplashView()
-                .task {
-                    await appState.checkLoginStatus()
+                .onAppear {
+                    Task {
+                        await appState.checkLoginStatus(
+                            keychain: container.userSessionKeychain
+                        )
+                    }
                 }
             
-
         case .auth:
             AuthFlowView {
                 appState.route = .main // 인증/온보딩 끝
             }
             .environmentObject(appState)
-
+            
         case .main:
             AppFlowView()
                 .environmentObject(appState)
-
+            
         }
     }
 }
